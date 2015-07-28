b9ee3b9d21e79bc7e400e62fe61b0dd1357a0467
Jeff Maher <jeff.maher@va.gov>
2015-07-28 15:41:08 -0400

    Adding a shell script to the setup environment

---
+++ env.sh
@@ -0,0 +1,14 @@
+# eFolder URL
+export CONNECT_VBMS_URL="***REMOVED***"
+
+export CONNECT_VBMS_PATH="/Users/vacomaherj/GitHub/deployment/decrypted_creds/test"
+export CONNECT_VBMS_TIER="test"
+export FILE_PREFIX="$CONNECT_VBMS_PATH/caseflow_$CONNECT_VBMS_TIER"
+
+# JKS File Path
+export CONNECT_VBMS_KEYFILE="${FILE_PREFIX}_jks"
+export CONNECT_VBMS_SAML="${FILE_PREFIX}_saml"
+#export CONNECT_VBMS_KEY="$CONNECT_VBMS_PATH"
+export CONNECT_VBMS_KEYPASS="importkey"
+#export CONNECT_VBMS_CACERT=
+#export CONNECT_VBMS_CERT=
