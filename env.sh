e53b91817094daf85e3e60237a172ba54f66afb1
Jeff Maher <jeff.maher@va.gov>
2015-07-29 07:55:35 -0400

    Cleaned up env.sh

--- env.sh
+++ env.sh
@@ -1,14 +1,10 @@
-# eFolder URL
+export JRUBY_OPTS="--2.0"
 export CONNECT_VBMS_URL="***REMOVED***"
-
-export CONNECT_VBMS_PATH="/Users/vacomaherj/GitHub/deployment/decrypted_creds/test"
-export CONNECT_VBMS_TIER="test"
-export FILE_PREFIX="$CONNECT_VBMS_PATH/caseflow_$CONNECT_VBMS_TIER"
-
-# JKS File Path
-export CONNECT_VBMS_KEYFILE="${FILE_PREFIX}_jks"
-export CONNECT_VBMS_SAML="${FILE_PREFIX}_saml"
-#export CONNECT_VBMS_KEY="$CONNECT_VBMS_PATH"
+export CONNECT_VBMS_KEYFILE="/Users/vacomaherj/GitHub/deployment/decrypted_creds/test/caseflow_test_jks"
+export CONNECT_VBMS_SAML="/Users/vacomaherj/GitHub/deployment/decrypted_creds/test/caseflow_test_saml"
 export CONNECT_VBMS_KEYPASS="importkey"
+
+# --- Production Only ---
+#export CONNECT_VBMS_KEY="$CONNECT_VBMS_PATH"
 #export CONNECT_VBMS_CACERT=
 #export CONNECT_VBMS_CERT=
