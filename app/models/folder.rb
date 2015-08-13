acf1fe4001e9e8ae5281fae4afadc1e24b076f6c
Alex Gaynor <alex.gaynor@gmail.com>
2015-08-13 10:24:59 -0400

    one more

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -6,8 +6,8 @@ class Folder < ActiveRecord::Base
 
   belongs_to :correspondent, foreign_key: :ticorkey, primary_key: :stafkey
 
-  belongs_to :added_by, class: StaffLocation, foreign_key: :tiaduser
-  belongs_to :changed_by, class: StaffLocation, foreign_key: :timduser
+  belongs_to :added_by, class_name: StaffLocation, foreign_key: :tiaduser
+  belongs_to :changed_by, class_name: StaffLocation, foreign_key: :timduser
 
   has_one :case, foreign_key: :bfkey
 
