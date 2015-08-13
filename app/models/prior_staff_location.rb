9d5c8332c1b44989c470188aab16aaf498242a7b
Alex Gaynor <alex.gaynor@gmail.com>
2015-08-13 10:24:11 -0400

    Use class_name, which is the correct name of this param

--- app/models/prior_staff_location.rb
+++ app/models/prior_staff_location.rb
@@ -5,7 +5,7 @@ class PriorStaffLocation < ActiveRecord::Base
   belongs_to :case, foreign_key: :lockey
   belongs_to :folder, foreign_key: :lockey
 
-  has_one :sent_by, class: StaffLocation, foreign_key: :stafkey, primary_key: :locstto
-  has_one :received_by, class: StaffLocation, foreign_key: :stafkey, primary_key: :locstrcv
-  has_one :checked_out_by, class: StaffLocation, foreign_key: :stafkey, primary_key: :locstout
+  has_one :sent_by, class_name: StaffLocation, foreign_key: :stafkey, primary_key: :locstto
+  has_one :received_by, class_name: StaffLocation, foreign_key: :stafkey, primary_key: :locstrcv
+  has_one :checked_out_by, class_name: StaffLocation, foreign_key: :stafkey, primary_key: :locstout
 end
--- app/models/staff_location.rb
+++ app/models/staff_location.rb
@@ -2,17 +2,17 @@ class StaffLocation < ActiveRecord::Base
   self.table_name = 'vacols.staff'
   self.primary_key = 'stafkey'
 
-  has_many :all_assignments, class: Assignment, foreign_key: :tskstfas
-  has_many :open_assignments, -> { where 'tskdcls is null' }, class: Assignment, foreign_key: :tskstfas
-  has_many :closed_assignments, -> { where 'tskdcls is not null' }, class: Assignment, foreign_key: :tskstfas
+  has_many :all_assignments, class_name: Assignment, foreign_key: :tskstfas
+  has_many :open_assignments, -> { where 'tskdcls is null' }, class_name: Assignment, foreign_key: :tskstfas
+  has_many :closed_assignments, -> { where 'tskdcls is not null' }, class_name: Assignment, foreign_key: :tskstfas
 
-  has_many :added_assignments, class: Assignment, foreign_key: :tskadusr
-  has_many :owned_assignments, class: Assignment, foreign_key: :tskstown
-  has_many :changed_assignments, class: Assignment, foreign_key: :tskmdusr
+  has_many :added_assignments, class_name: Assignment, foreign_key: :tskadusr
+  has_many :owned_assignments, class_name: Assignment, foreign_key: :tskstown
+  has_many :changed_assignments, class_name: Assignment, foreign_key: :tskmdusr
 
   has_many :attachments, foreign_key: :imgowner
-  has_many :added_attachments, class: Attachment, foreign_key: :imgadusr
-  has_many :changed_attachments, class: Attachment, foreign_key: :imgmdusr
+  has_many :added_attachments, class_name: Attachment, foreign_key: :imgadusr
+  has_many :changed_attachments, class_name: Attachment, foreign_key: :imgmdusr
 
-  has_many :current_cases, class: Case, foreign_key: :bfcurloc
+  has_many :current_cases, class_name: Case, foreign_key: :bfcurloc
 end
