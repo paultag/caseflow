53a301dc962193ac973cf65131ba932e2af4b626
Alex Gaynor <alex.gaynor@gmail.com>
2015-08-13 10:32:09 -0400

    Use the VBMS::Client's from_env_vars for configuration

--- config/initializers/connect_vbms.rb
+++ config/initializers/connect_vbms.rb
@@ -2,12 +2,4 @@
 
 require 'vbms'
 
-$vbms = VBMS::Client.new(
-    ENV['CONNECT_VBMS_URL'],
-    ENV['CONNECT_VBMS_KEYFILE'],
-    ENV['CONNECT_VBMS_SAML'],
-    ENV['CONNECT_VBMS_KEY'],
-    ENV['CONNECT_VBMS_KEYPASS'],
-    ENV['CONNECT_VBMS_CACERT'],
-    ENV['CONNECT_VBMS_CERT']
-  )
+$vbms = VBMS::Client.from_env_vars(env_name: ENV["CONNECT_VBMS_ENV"])
