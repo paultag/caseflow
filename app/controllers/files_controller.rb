040c467bcaeb00c23dfdf676c0cfbe4a03dc3b00
Alex Gaynor <alex.gaynor@gmail.com>
2015-09-09 10:48:22 -0400

    Fixed #125 -- don't allow arbitrary file dislcosure.

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -3,6 +3,9 @@ class FilesController < ApplicationController
 
   def show
     @filepath = Rails.root + 'tmp' + params[:type] + [params[:id], params[:format]].join('.')
+    if !@filepath.start_with?(Rails.root + 'tmp')
+      head :not_found
+    end
 
     send_file(@filepath, filename: [params[:id], 'pdf'].join('.'), type: 'application/pdf')
   rescue
