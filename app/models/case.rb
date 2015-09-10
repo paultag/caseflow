f8194ec06fa3279c683cbc2d56a3813739b79a89
Jeff Maher <jeff.maher@va.gov>
2015-09-10 13:19:16 -0400

    Made ready_to_certify? accessible to both fake and real cases

--- app/models/case.rb
+++ app/models/case.rb
@@ -56,6 +56,17 @@ module Caseflow
 
     fields
   end
+
+  def self.ready_to_certify?(kase)
+    kase.bfdnod_date == kase.efolder_nod_date &&
+        kase.bfdsoc_date == kase.efolder_soc_date &&
+        kase.bfssoc1_date == kase.efolder_ssoc1_date &&
+        kase.bfssoc2_date == kase.efolder_ssoc2_date &&
+        kase.bfssoc3_date == kase.efolder_ssoc3_date &&
+        kase.bfssoc4_date == kase.efolder_ssoc4_date &&
+        kase.bfssoc5_date == kase.efolder_ssoc5_date &&
+        kase.bfd19_date == kase.efolder_form9_date
+  end
 end
 
 class Case < ActiveRecord::Base
@@ -113,16 +124,9 @@ class Case < ActiveRecord::Base
   def required_fields
     Caseflow.required_fields_for_case(self)
   end
-
+  
   def ready_to_certify?
-    bfdnod_date == efolder_nod_date &&
-        bfdsoc_date == efolder_soc_date &&
-        bfssoc1_date == efolder_ssoc1_date &&
-        bfssoc2_date == efolder_ssoc2_date &&
-        bfssoc3_date == efolder_ssoc3_date &&
-        bfssoc4_date == efolder_ssoc4_date &&
-        bfssoc5_date == efolder_ssoc5_date &&
-        bfd19_date == efolder_form9_date
+    Caseflow.ready_to_certify?(self)
   end
 
   def appeal_type
