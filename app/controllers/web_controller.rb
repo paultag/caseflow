2093f0fed1d5c34cb5a7e4a8f8507e5299f4221a
Jeff Maher <jeff.maher@va.gov>
2015-09-21 12:42:49 -0400

    Fixed form 8 question 13 answers

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -58,6 +58,12 @@ class WebController < ApplicationController
       fields.delete('11B_HAVE_THE_REQUIREMENTS_OF_38_USC_BEEN_FOLLOWED')
     end
 
+    fields.each do |key, val|
+      if key =~ /^13_RECORDS_TO_BE_FORWARDED_TO_BOARD_OF_VETERANS_APPEALS_/ && val == 'on'
+        fields[key] = true
+      end
+    end
+
     # Generate Form 8 PDF
     form_8 = FormVa8.new(fields)
     form_8.process!
