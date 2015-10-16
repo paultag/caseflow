01aeba1457a1f0b913630794fb2ea0cab52a779d
Alex Gaynor <alex.gaynor@gmail.com>
2015-10-16 14:52:05 -0400

    write some tests for safe_join

--- test/controllers/caseflow_test.rb
+++ test/controllers/caseflow_test.rb
@@ -13,4 +13,12 @@ class CaseflowTest < ActiveSupport::TestCase
     assert !Caseflow.is_child_path?(base, base + "..")
     assert !Caseflow.is_child_path?(base, base + "../cd")
   end
+
+  test "safe_join" do
+    base = Pathname.new("/a/b/c")
+
+    assert_equal Caseflow.safe_join(base, "abc.txt"), Pathname.new("/a/b/c/abc.txt")
+    assert Caseflow.safe_join(base, "..").nil?
+    assert Caseflow.safe_join(base, "../cd").nil?
+  end
 end
