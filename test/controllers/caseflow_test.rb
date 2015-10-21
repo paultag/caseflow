6b154ab4fa9456041891d4603bf10e8bb03f8855
Jeff Maher <jeff.maher@va.gov>
2015-10-21 07:50:56 -0400

    Test setup

--- test/controllers/caseflow_test.rb
+++ test/controllers/caseflow_test.rb
@@ -14,11 +14,32 @@ class CaseflowTest < ActiveSupport::TestCase
     assert !Caseflow.is_child_path?(base, base + "../cd")
   end
 
-  test "safe_join" do
+  test 'safe_join' do
     base = Pathname.new("/a/b/c")
 
     assert_equal Caseflow.safe_join(base, "abc.txt"), Pathname.new("/a/b/c/abc.txt")
     assert_nil Caseflow.safe_join(base, "..")
     assert_nil Caseflow.safe_join(base, "../cd")
   end
+
+  test 'no remarks stays as no remarks' do
+
+  end
+
+  test 'short one line remark does not get split' do
+
+  end
+
+  test 'more than one line remark gets split' do
+
+  end
+
+  test 'long one line remark gets split' do
+
+  end
+
+  test 'remark with long first line gets split correctly' do
+
+  end
+
 end
