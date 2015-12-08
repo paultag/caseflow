ae6c377a283b162bd8ec42ffb2681f1ea585cd7a
Tiffany Brown <tiffany@webinista.com>
2015-12-08 12:17:57 -0500

    Hiding 11B if 'no' is selected for 11A.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,27 +4,6 @@
  */
  //= require_self
 
-/* Copies appeals ID to clipboard */
-(function () {
-     "use strict";
-     new Clipboard('[data-clipboard-text]');
- })();
-
-/* Reusable 'go back one page' pattern */
- $(function() {
-    $('.cf-action-back').on('click', function(evt) {
-        window.history.back();
-    });
- });
-
-/* Reusable 'refresh' pattern */
-$(function() {
-    $('.cf-action-refresh').on('click', function(evt) {
-        location.reload(); return false;
-    });
-});
-
-
 /*
 Extends jQuery to add a toggleAttr method
 https://gist.github.com/mathiasbynens/298591
@@ -53,8 +32,49 @@ $.fn.extend({
     }
 });
 
-$(function(){
+/* Copies appeals ID to clipboard */
+(function () {
+     "use strict";
+     new Clipboard('[data-clipboard-text]');
+ })();
 
+/* Reusable 'go back one page' pattern */
+ $(function() {
+    $('.cf-action-back').on('click', function(evt) {
+        window.history.back();
+    });
+ });
+
+/* Reusable 'refresh' pattern */
+$(function() {
+    $('.cf-action-refresh').on('click', function(evt) {
+        location.reload(); return false;
+    });
+});
+
+/* Reusable 'modal' pattern */
+$(function() {
+    $('.cf-action-openmodal').on('click', function(e) {
+        var toopen = $(e.target).attr('href');
+        $(toopen).openItem();
+    });
+
+    $('.cf-action-close').on('click', function(e) {
+        var toclose = $(e.target).data('controls');
+        $(toclose).closeItem();
+
+        /*
+        Since this may be a modal shown using :target,
+        we should update the hash to close it.
+        */
+
+        if(window.location.hash) {
+            window.location.hash = '';
+        }
+    });
+});
+
+$(function(){
     /* Trigger for the dropdown */
     $(".dropdown-trigger").on('click', function(e) {
          e.preventDefault(); // Prevent page jump
@@ -65,6 +85,23 @@ $(function(){
     $(":not(.dropdown)").on('click', function(e){
         if( !$(e.target).parents('.dropdown').length ) {
             $('.dropdown-menu').closeItem();
+        }
+    });
+});
+
+$(function(){
+    $('fieldset').on('change', function(e){
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+
+        var whichEl = $(this).data('linkedto'),
+            showwhen = $(this).data('showwhen');
+
+        if( showwhen == $(e.target).attr('id') ||
+            showwhen == $(e.target).attr('name') ){
+            $(whichEl).closeItem();
+        } else {
+            $(whichEl).openItem();
         }
     });
 });
