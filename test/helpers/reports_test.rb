14bcef916df4a69cb1357fb1699cd6a03b707cf7
Alex Gaynor <alex.gaynor@gmail.com>
2015-12-30 16:47:48 -0500

    Added very simplistic tests for reports.
    Just verifies that calling their spreadsheet_cells method doesn't error

--- test/helpers/reports_test.rb
+++ test/helpers/reports_test.rb
@@ -18,4 +18,14 @@ class ReportsTest < ActiveSupport::TestCase
     f = Caseflow::Fakes::Case.new(bfdnod: t1, bfd19: t2, efolder_soc: t2)
     assert_equal Caseflow::Reports.mismatched_dates(f), "NOD, Form 9, SOC"
   end
+
+  test "SeamReport.spreadsheet_cells doesn't error"
+    c = Caseflow::Fakes::Case.new
+    SeamReport.new.spreadsheet_cells(c)
+  end
+
+  test "MismatchedDocumentsReport.spreadsheet_cells doesn't error"
+    c = Caseflow::Fakes::Case.new
+    MismatchedDocumentsReport.new.spreadsheet_cells(c)
+  end
 end
