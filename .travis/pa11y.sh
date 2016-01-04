ab3477973f49b805bad04bbbdc32d3d71faed1b9
Alex Gaynor <alex.gaynor@gmail.com>
2016-01-04 10:21:47 -0500

    use an array so that adding additional urls is easy

--- .travis/pa11y.sh
+++ .travis/pa11y.sh
@@ -11,6 +11,10 @@ while ! nc -z -w1 localhost 3000 > /dev/null; do
   sleep 1
 done
 
-pa11y --standard=Section508 --level=warning http://localhost:3000/caseflow/login
+URLS=(/caseflow/login)
+
+for url in in ${URLS[@]}; do
+  pa11y --standard=Section508 --level=warning "http://localhost:3000$url"
+done
 
 kill $RAILS_PID
