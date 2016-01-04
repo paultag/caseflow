859e6d87be1d3515403ccbc51608395a09884495
Alex Gaynor <alex.gaynor@gmail.com>
2016-01-04 09:35:25 -0500

    make this more realistic

--- test/helpers/reports_test.rb
+++ test/helpers/reports_test.rb
@@ -20,7 +20,7 @@ class ReportsTest < ActiveSupport::TestCase
   end
 
   test "SeamReport.spreadsheet_cells doesn't error" do
-    c = Caseflow::Fakes::Case.new(folder: Caseflow::Fakes::Folder.new("VBMS"))
+    c = Caseflow::Fakes::Case.new(folder: Caseflow::Fakes::Folder.new("Paper"))
     Caseflow::Reports::SeamReport.new.spreadsheet_cells(c)
   end
 
