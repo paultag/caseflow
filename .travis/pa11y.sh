5537f7eba1e5f2746a94ff583c2a560c339f25a7
Alex Gaynor <alex.gaynor@gmail.com>
2016-01-04 10:18:56 -0500

    Move this to a script

---
+++ .travis/pa11y.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -x
+
+./bin/rails s &
+RAILS_PID=$!
+
+while ! nc -z -w1 localhost 3000 > /dev/null; do
+  sleep 1
+done
+
+pa11y --standard=Section508 --level=warning http://localhost:3000/caseflow/login
+
+kill $RAILS_PID
